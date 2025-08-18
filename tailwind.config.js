ebc0df7eac47e1492517ca386fb9964b9a1ac437
bluenoe <baokhanh.dev281@gmail.com>
2025-08-18 19:46:44 +0700

    build: add Tailwind CSS configuration and dependencies
    - Add postcss.config.js and tailwind.config.js configuration files
    - Install Tailwind CSS, PostCSS, Autoprefixer and line-clamp plugin
    - Replace index.css with Tailwind directives

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+import lineClamp from '@tailwindcss/line-clamp'
+export default {
+  darkMode: 'media',
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: { extend: {} },
+  plugins: [lineClamp],
+}
